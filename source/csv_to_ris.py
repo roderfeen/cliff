a6153638c020ddaee30367b12d7767475ec9e873
roderfeen <roderfeen@pm.me>
2022-05-30 10:04:35 +0200

    Created screening_tagger
    * Created screening tagger to tag asreview items in order
    * Fixed issues with ris_filter
    * Aesthetic changes to csv_to_ris.py

--- source/csv_to_ris.py
+++ source/csv_to_ris.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 import pandas as pd
 import argparse, os, sys, json
+
+'''Converts an output asreview csv file to ris such that it can be imported into zotero'''
+
 #
 # Define functions
 #
-# def invert_dict(dict):
-# '''old: was used to convert dict found online to reverse ris_labels dict required'''
-#     return {v: k for k, v in dict.items()}
-
 def format_authors(authors):
     '''
     Change authors format type to match that required by zotero, yielding a list of authors which will each be put on a separate line in the ris file outputted by the script
@@ -26,10 +25,10 @@ def csv_to_ris(input, output, ris_labels):
     * map ris tag names to column names
     * create new file in ris format on a per row basis
     '''
-    df = pd.read_csv(input, sep=';')
+    df = pd.read_csv(input, sep=',')
     # col_names = list(df.columns.values)
-    include_columns = ['type_of_reference', 'title', 'authors', 'secondary_title', 'date', 'year', 'doi', 'volume', 'number', 'issn', 'short_title', 'url', 'access_date', 'keywords', 'abstract', 'start_page', 'end_page', 'accession_number', 'asreview_ranking']  #asreview_ranking is included under ris label N1 - 'notes'
-    # exclude_columns = ['record_id', 'database_provider', 'alternate_title1','language','secondary_authors', 'note', 'publisher']  # for reference
+    include_columns = ['type_of_reference', 'title', 'authors', 'secondary_title', 'date', 'year', 'doi', 'volume', 'number', 'issn', 'short_title', 'url', 'keywords', 'abstract', 'start_page', 'end_page', 'accession_number', 'asreview_ranking']  #asreview_ranking is included under ris label N1 - 'notes'
+    # exclude_columns = ['record_id', 'database_provider', 'alternate_title1','language','secondary_authors', 'note', 'publisher', 'access date']  # for reference
 
     df['authors'] = df['authors'].apply(format_authors)
 
@@ -46,10 +45,10 @@ def csv_to_ris(input, output, ris_labels):
                                 line = f'{ris_labels[col]}  - {au}\n'  # 'author' = AU
                                 risfile.write(line)
                         else:
-                            line = f'{ris_labels[col]}  - {value.item()}\n'  # 'author' = AU
+                            line = f'{ris_labels[col]}  - {value.item()}\n'
                             risfile.write(line)
             
-                risfile.write('ER  - \n')
+                risfile.write('ER  - \n\n')  # Ris entries are split by ER -
     
     return risfile
 
@@ -72,10 +71,10 @@ if not os.path.isfile(args.input):
     print('The file specified does not exist')
     sys.exit()
 
-# Import ris labels dict from json
+# Import ris labels dict from json (TODO: Fix)
 # with open("ris_labels.json", "r") as in_handle:
 #     ris_labels = json.load(in_handle)  # load dictionary
-
+# For now just put label data here
 ris_labels = {"first_authors": "A1", "secondary_authors": "A2", "tertiary_authors": "A3", "subsidiary_authors": "A4", "abstract": "AB", "author_address": "AD", "accession_number": "AN", "authors": "AU", "custom1": "C1", "custom2": "C2", "custom3": "C3", "custom4": "C4", "custom5": "C5", "custom6": "C6", "custom7": "C7", "custom8": "C8", "caption": "CA", "asreview_ranking": "CN", "place_published": "CY", "date": "DA", "name_of_database": "DB", "doi": "DO", "database_provider": "DP", "end_page": "EP", "end_of_reference": "ER", "edition": "ET", "id": "ID", "number": "IS", "alternate_title1": "J2", "alternate_title2": "JA", "alternate_title3": "JF", "journal_name": "JO", "keywords": "KW", "file_attachments1": "L1", "file_attachments2": "L2", "figure": "L4", "language": "LA", "label": "LB", "note": "M1", "type_of_work": "M3", "notes": "N1", "notes_abstract": "N2", "number_of_volumes": "NV", "original_publication": "OP", "publisher": "PB", "year": "PY", "reviewed_item": "RI", "research_notes": "RN", "reprint_edition": "RP", "section": "SE", "issn": "SN", "start_page": "SP", "short_title": "ST", "primary_title": "T1", "secondary_title": "T2", "tertiary_title": "T3", "translated_author": "TA", "title": "TI", "translated_title": "TT", "type_of_reference": "TY", "unknown_tag": "UK", "url": "UR", "volume": "VL", "publication_year": "Y1", "access_date": "Y2"}
 
 #
